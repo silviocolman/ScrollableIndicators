e45dbecd25e697160d7a3e0fad89751048617fd1
Silvio Colman <silviorafael.colman@gmail.com>
2024-04-22 17:15:43 -0400

    Initial Commit

---
+++ ScrollableIndicators/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScrollableIndicators
+//
+//  Created by Silvio Colmán on 2024-04-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScrollableIndicators/ScrollableIndicatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollableIndicatorsApp.swift
+//  ScrollableIndicators
+//
+//  Created by Silvio Colmán on 2024-04-22.
+//
+
+import SwiftUI
+
+@main
+struct ScrollableIndicatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
